8918cf7e6368b0597e3150f60d5b6db8a86aa005
sanjeev <sanjeevsheoran07@gmail.com>
2023-06-25 23:25:13 +0530

    login change

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -2,13 +2,13 @@
     <div class="container mt-3 w-25 text-center">
         <form action="/">
             <div class="form-group">
-                <input type="text" class="form-control mt-3" placeholder="Enter Username" />
+                <input type="email" class="form-control mt-3" placeholder="Enter Username" required/>
             </div>
             <div class="form-group">
-                <input type="Password" class="form-control mt-3" placeholder="Enter Password" />
+                <input type="Password" class="form-control mt-3" placeholder="Enter Password" required/>
             </div>
             <div class="form-group">
-                <a class="form-control btn btn-primary mt-3" href="/" value="Log In">Login</a>
+                <input class="form-control btn btn-primary mt-3" type="submit" href="/" value="Log In"/>
             </div>
         </form>
     </div>
